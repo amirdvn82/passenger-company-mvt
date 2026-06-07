56e132a10aae7ff77e407f24df054306456e2dac
Amir <amir.dvn.82@gmail.com>
2026-06-07 13:05:05 +0330

    feat(wallets): implement wallet and wallet transaction models
    - add Wallet model with one-to-one relation to user
    - implement WalletTransaction for financial history tracking
    - support deposit and withdraw transaction types

--- cooperative/cooperative/settings.py
+++ cooperative/cooperative/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'cities',
     'trips',
     'tickets',
+    'wallets',
 ]
 
 MIDDLEWARE = [
---
+++ cooperative/wallets/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.conf import settings
+
+
+class Wallet(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='wallet')
+    balance = models.PositiveBigIntegerField(default=0)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.username} Wallet'
+
+
+class WalletTransaction(models.Model):
+
+    class TransactionType(models.TextChoices):
+        DEPOSIT = 'DEPOSIT', 'Deposit'
+        WITHDRAW = 'WITHDRAW', 'Withdraw'
+
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name='transactions')
+
+    amount = models.DecimalField(max_digits=12, decimal_places=0)
+    transaction_type = models.CharField( max_length=20, choices=TransactionType.choices)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.wallet.user.username} - {self.get_transaction_type_display()} - {self.amount}"
+
