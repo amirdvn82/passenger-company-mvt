717098006491f25318ccab2ac14bb7887fdaea6b
Amir <amir.dvn.82@gmail.com>
2026-06-17 02:12:52 +0330

    fix: properly register TripAdmin in Django admin

--- cooperative/trips/admin.py
+++ cooperative/trips/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Trip
 
-admin.site.register(Trip)
+@admin.register(Trip)
 class TripAdmin(admin.ModelAdmin):
     list_display = ('id', 'origin', 'destination', 'departure_time', 'driver', 'status', 'price')
     list_filter = ('status', 'departure_time')
